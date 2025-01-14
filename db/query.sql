151cd7a5687a6cfeb8dcb6626fa2b6c16202b9d3
Papaya <stephaniesosa02@gmail.com>
2025-01-13 22:27:43 -0700

    bug fixed

--- db/query.sql
+++ db/query.sql
@@ -1,19 +1,18 @@
 SELECT * FROM department;
-SELECT * FROM roles;
+SELECT * FROM role;
 SELECT * FROM employee;
 SELECT * FROM manager;
 
-SELECT role_id, title, salary, department.names FROM roles
-JOIN department ON roles.department_id = department.id;
+SELECT role_id, title, salary, department.name FROM role
+JOIN department ON role.department_id = department.id;
 
 SELECT 
 employee.id, employee.first_name AS "Employee First Name", 
 employee.last_name AS "Employee Last Name",
-role.title, department.names AS department,
-role.salary, CONCAT(manager.first_name, ' ', manager.last_name) --remove name?
+role.title, department.name AS department,
+role.salary, CONCAT(manager.first_name, ' ', manager.last_name)
 FROM employee
-INNER JOIN roles ON employee.role_id = role.id
+INNER JOIN role ON employee.role_id = role.id
 INNER JOIN department ON role.department_id = department.id
 LEFT JOIN employee AS manager ON employee.manager_id = manager.id;
 
--- ^come back to this later
